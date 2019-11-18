ad30a2b292e55ac25c7079483be72135727299f4
mayank wankhede <mayankwankhede@gmail.com>
2019-11-18 16:32:12 +0530

    test db

---
+++ Desktop/docker_client_server/db/data.sql
@@ -0,0 +1,3 @@
+create table student(id int primary key auto_increment,name varchar(100),city varchar(100));
+
+insert into student(name,city) values ('mayank','nagpur'),('chetan','wardha'),('madhu','gondia');
